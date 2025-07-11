2aafa2799c3c715cb1baa855cf59b307c8c6748b
heitor.braga <heitor.pereira@fockink.ind.br>
2025-07-11 13:15:34 -0300

    :construction: wip - progress

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/heitorBragaPereira/jwt-authentication
+module jwt-authentication
 
 go 1.22.6
+
+require github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
---
+++ internal/service/jwt.go
@@ -0,0 +1,27 @@
+package service
+
+import (
+	"time"
+
+	auth "jwt-authentication/internal/model"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func GenerateJWT(userID, email string) (string, error) {
+	claims := auth.CustomClaims{
+		UserID: userID,
+		Email:  email,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := token.SignedString([]byte("sua-chave-secreta"))
+	if err != nil {
+		return "", err
+	}
+	return signedToken, nil
+}
