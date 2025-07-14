7f2a6921c38afe4a5e4035f7c724c2d92b986238
heitor.braga <heitor.pereira@fockink.ind.br>
2025-07-14 13:33:33 -0300

    :construction: wip

--- web/jwt-authentication/src/app/page.tsx
+++ web/jwt-authentication/src/app/page.tsx
@@ -1,10 +1,15 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import path from "@/assets/logo.svg";
 
 export default function LoginPage() {
   return (
     <div className="w-full h-screen flex justify-center items-center">
-      <div className="w-[300px] h-[350px] flex flex-col">
+      <div className="w-[350px] flex flex-col justify-center gap-4 bg-secondary px-4 py-6 border border-gray-300">
+        <div className="w-full flex justify-center">
+          <Image src={path} alt="Logo" width={100} />
+        </div>
         <Input />
         <Input />
         <Button>Entrar</Button>
